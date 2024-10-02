92f66c48cc770acdff03bfee88082128067fc59a
Irdgyu <gilangbayuirwana@gmail.com>
2024-10-02 20:36:06 +0700

    Hayo ... genap apa ganjil nih?

--- PemilihanBilangan06.java
+++ PemilihanBilangan06.java
@@ -4,16 +4,17 @@ public class PemilihanBilangan06 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Masukkan sebuah angka : ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + "termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + "termasuk bilangan ganjil");
-        }
+        System.out.print("Masukkan angka pembagi : ");
+        int bilangan = sc.nextInt();
+        int nomor = angka / bilangan;
 
+        System.out.println("Jika angka " + angka + " dibagi dengan " + bilangan + " maka hasilnya adalah " + nomor);
+        String hasil = (nomor % 2 == 0) ? "Sehingga angka " + nomor + " termasuk ke dalam bilangan genap " :
+        "Sehingga angka " + nomor + " termasuk ke dalam bilangan ganjil ";
+
+        System.out.println(hasil);
     }
 }
